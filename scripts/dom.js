3a7d4dcb60a13880415a05b9e4e06a404b35327d
Usuario <usuario@MBP-de-Usuario.fibertel.com.ar>
2021-07-26 21:33:01 -0300

    day by day

--- scripts/dom.js
+++ scripts/dom.js
@@ -3,8 +3,10 @@ function sectionSearch() {
 <div id="div-search">
     <h1>Inspírate, busca, guarda, y crea los mejores <span>GIFOS</span></h1>
     <img src="./assets/imgs/main-img.svg" alt="main-img">
-    <input type="text">
-    <img src="./assets/imgs/icon-search.svg" alt="icon-search">
+    <div class="input-container">
+      <input type="text" placeholder="Busca GIFOS y más">
+      <img id="icon-search" src="./assets/imgs/icon-search.svg" alt="icon-search">
+    </div>
     <div id="section-main-footer">
         <h2>Trendings:</h2>
         <p>Reactions, Entertainment, Sports, Stickers, Artists</p>
