01e2d5c79f1b3a5e2f4456fca0fd4c13c556473a
Usuario <usuario@MBP-de-Usuario.fibertel.com.ar>
2021-07-29 20:09:23 -0300

    fix css bugs && start on trends js

--- scripts/create-gif.js
+++ scripts/create-gif.js
@@ -1,14 +1,14 @@
 async function createGif(data, parent) {
-  let gifs = await data;
+    let gifs = await data;
   gifs.data.forEach((gif) => {
     const dom = `
     <div class="gif">
         <img src="${gif.images.fixed_height.url}" alt="${gif.title}">
         <div class="gif-hover">
             <div class="btns">
-                <button>DOWNLOAD</button>
-                <button>EXPAND</button>
-                <button>SAVE</button>
+                <img class="gif-btn" src="./assets/imgs/btn-gif-favs.svg" atl="btn-gif-fav">
+                <img class="gif-btn" src="./assets/imgs/btn-gif-download.svg" atl="btn-gif-download">
+                <img class="gif-btn" src="./assets/imgs/btn-gif-expand.svg" atl="btn-gif-expand">
             </div>
             <div class="info">
                 <span class="user-name">${gif.username}</span>
